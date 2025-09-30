d279e345a963163072dd34785a749806aba3a393
Yashvesh20 <yashvesh20@gmail.com>
2025-09-30 20:38:33 +0530

    Create Linked_list.cpp

---
+++ Linked_list.cpp
@@ -0,0 +1,25 @@
+Yashvesh Singh 24070123138
+#include<iostream>
+using namespace std;
+
+class Link {
+    public:
+    int d;
+    Link *next;
+
+    Link(int num){
+        d=num;
+        next=NULL;
+    }
+    /*void disp(){
+        cout<<d<<"   "<<next;
+    }*/
+};
+
+int main(){
+    Link*l1=new Link(4);
+    cout<<l1->d<<"   "<<l1->next;
+}
+
+output:
+4   0
